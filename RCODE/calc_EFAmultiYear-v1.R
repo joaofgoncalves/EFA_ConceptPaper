b5744c32632afb5337ea88f713eb5b21d1bd1240
João F Gonçalves <joaofgo@gmail.com>
2019-07-22 12:04:31 +0100

    Changed multi-year calculations

--- RCODE/calc_EFAmultiYear-v1.R
+++ RCODE/calc_EFAmultiYear-v1.R
@@ -3,24 +3,28 @@
 library(raster)
 
 getLmResid <- function(x, y=2001:2018, na.rm=TRUE){
-  
   if(na.rm){
     if(any(is.na(x)|is.nan(x)|is.null(x))){
-      return(NA)
+      return(rep(NA,length(x)))
     } 
   }
   DF <- data.frame(x = x, y = y)
-  mod <- lm(x~y, data = DF)
-  return(residuals(mod))
+  mod <- try(lm(x~y, data = DF), silent = TRUE)
+  if(inherits(mod, "try-error")){
+    return(rep(NA,length(x)))  
+  }else{
+    return(residuals(mod))
+  }
 }
 
 ## -------------------------------------------------------------------------------------------- ##
 
-dirsToParse <- list.dirs("./DATA/RASTER/EFA_ANNUAL", recursive = FALSE)
+dirsToParse <- list.dirs("./DATA/RASTER/EFA_ANNUAL_v2", recursive = FALSE)
 
 pb <- txtProgressBar(1,length(dirsToParse),style=3)
 
 i <- 0
+
 for(Dir in dirsToParse){
   
   i <- i + 1
@@ -29,28 +33,34 @@ for(Dir in dirsToParse){
   rstFilePath <- list.files(Dir, pattern = ".tif$", full.names = TRUE)
   rstStack <- stack(rstFilePath)
   
-  rstAvg <- calc(rstStack, mean)
-  rstStd <- calc(rstStack, sd)
+  rstAvg <- calc(rstStack, mean, na.rm=TRUE)
+  rstStd <- calc(rstStack, sd, na.rm=TRUE)
   rstCoV <- rstStd / rstAvg
-    
+  
+  writeRaster(rstAvg, paste("./DATA/RASTER/EFA_MULTI_YEAR/",varName,
+                            "_MultiYr_Avg_01_18_v1.tif",sep=""))
+  writeRaster(rstStd, paste("./DATA/RASTER/EFA_MULTI_YEAR/",varName,
+                            "_MultiYr_Std_01_18_v1.tif",sep=""))
+  writeRaster(rstCoV, paste("./DATA/RASTER/EFA_MULTI_YEAR/",varName,
+                            "_MultiYr_CoV_01_18_v1.tif",sep=""))
+  
   # De-trend annual EFA's based on linear regression
-  #
-  # detrdRst <- calc(rstStack, getLmResid)
-  # 
-  # detrdRstAvg <- calc(detrdRst, mean)
-  # detrdRstStd <- calc(detrdRst, sd)
-  # detrdRstCoV <- detrdRstAvg / detrdRstStd
-  # 
+
+  detrdRst <- calc(rstStack, getLmResid)
+
+  message("-> De-trend done for variable: ", varName, "...... \n\n")
   
-  writeRaster(rstAvg, paste("./DATA/RASTER/EFA_MULTI_YR/",varName,"_MultiYrAvg.tif",sep=""))
-  writeRaster(rstAvg, paste("./DATA/RASTER/EFA_MULTI_YR/",varName,"_MultiYrStd.tif",sep=""))
-  writeRaster(rstAvg, paste("./DATA/RASTER/EFA_MULTI_YR/",varName,"_MultiYrCoV.tif",sep=""))
-  
-  # writeRaster(detrdRstAvg, paste("./DATA/RASTER/EFA_MULTI_YR/",varName,"_MultiYrAvg_detrd.tif",sep=""))
-  # writeRaster(detrdRstStd, paste("./DATA/RASTER/EFA_MULTI_YR/",varName,"_MultiYrStd_detrd.tif",sep=""))
-  # writeRaster(detrdRstCoV, paste("./DATA/RASTER/EFA_MULTI_YR/",varName,"_MultiYrCoV_detrd.tif",sep=""))
-  # 
-  
+  detrdRstAvg <- calc(detrdRst, mean, na.rm=TRUE)
+  detrdRstStd <- calc(detrdRst, sd, na.rm=TRUE)
+  detrdRstCoV <- detrdRstStd / detrdRstAvg 
+
+  writeRaster(detrdRstAvg, paste("./DATA/RASTER/EFA_MULTI_YEAR/",varName,
+                                 "_MultiYr_Avg_01_18_v1_detrd.tif",sep=""))
+  writeRaster(detrdRstStd, paste("./DATA/RASTER/EFA_MULTI_YEAR/",varName,
+                                 "_MultiYr_Std_01_18_v1_detrd.tif",sep=""))
+  writeRaster(detrdRstCoV, paste("./DATA/RASTER/EFA_MULTI_YEAR/",varName,
+                                 "_MultiYr_CoV_01_18_v1_detrd.tif",sep=""))
+
   setTxtProgressBar(pb, i)
 }
 
